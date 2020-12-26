1fd73b0d2799047ed63064eb6b60361d310fc798
amata1219 <suzukiamata@gmail.com>
2020-12-26 23:45:22 +0900

    :recycle: wait中にログインしたプレイヤーに対応し魔界

--- src/amata1219/tab/customizer/PlayerListNameSwitchTask.java
+++ src/amata1219/tab/customizer/PlayerListNameSwitchTask.java
@@ -17,29 +17,19 @@ public class PlayerListNameSwitchTask implements Runnable {
     @Override
     public void run() {
         HashMap<Player, String> playersToOriginalPlayerListNames = new HashMap<>();
-        //プレイヤーと元のプレイヤーリストネームを結び付けて保持するためのマップ
 
         for (Player player : Bukkit.getOnlinePlayers()) {
-            //全オンラインプレイヤーを一人ひとり処理していく
             playersToOriginalPlayerListNames.put(player, player.getPlayerListName());
-            //元の名前をプレイヤーに結び付けて、マップに保持させておく
-
             player.setPlayerListName("NewName");
-            //プレイヤーに新しいプレイヤリストネームを設定する
         }
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            //遅延処理をする
             for (Player player : Bukkit.getOnlinePlayers()) {
-                //全オンラインプレイヤーを一人ひとり処理していく
-
                 String originalPlayerListName = playersToOriginalPlayerListNames.get(player);
-                //プレイヤーに結び付けておいた元の名前をマップから読み取る
-
-                player.setPlayerListName(originalPlayerListName);
-                //元の名前に再設定する
+                if (originalPlayerListName != null) {
+                    player.setPlayerListName(originalPlayerListName);
+                }
             }
         }, 20L);
-        //20tick(1秒後)に実行するよう指定する
     }
 }
